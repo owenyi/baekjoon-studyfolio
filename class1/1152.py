0a32b1b8425aa56cd8915c87bb57d88eb9190a5c
LAPTOP-9D74L3IT\oweny <owenyi3207@gmail.com>
2021-03-17 21:01:54 +0900

    Update ~2475

--- class1/1152.py
+++ class1/1152.py
@@ -1,8 +1,4 @@
-str = input()
-alpha = [0] * 26
-for x in str:
-    if x.isupper(): alpha[ord(x) - 65] += 1
-    elif x.islower(): alpha[ord(x) - 97] += 1
-max = max(alpha)
-if alpha.count(max) > 1: print('?')
-else: print(chr(alpha.index(max) + 65))
+A, B = map(int, input().split())
+if A > B: print('>')
+elif A < B: print('<')
+else: print("==")
--- class1/1157.py
+++ class1/1157.py
@@ -1,4 +1,8 @@
-A, B = map(int, input().split())
-if A > B: print('>')
-elif A < B: print('<')
-else: print("==")
+str = input()
+alpha = [0] * 26
+for x in str:
+    if x.isupper(): alpha[ord(x) - 65] += 1
+    elif x.islower(): alpha[ord(x) - 97] += 1
+max = max(alpha)
+if alpha.count(max) > 1: print('?')
+else: print(chr(alpha.index(max) + 65))
