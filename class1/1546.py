6fd2101c86f0f1b06968b6cc473fcaafe7a3ab88
LAPTOP-9D74L3IT\oweny <owenyi3207@gmail.com>
2021-03-27 18:26:44 +0900

    update 1546

--- class1/1546.py
+++ class1/1546.py
@@ -1,6 +1,6 @@
 N = int(input())
 scores = list(map(int, input().split()))
 maxScore = max(scores)
-for i in range(len(scores)):
+for i in range(N):
     scores[i] = scores[i] / maxScore * 100
 print(sum(scores) / N)
