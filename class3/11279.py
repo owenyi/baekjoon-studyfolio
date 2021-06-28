ee13b2f1c25f19f3e04b255ff85dd7cde7a41684
owenyi <67588446+owenyi@users.noreply.github.com>
2021-06-28 22:10:10 +0900

    update class3 heapq

---
+++ class3/11279.py
@@ -0,0 +1,12 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+heap = []
+N = int(input())
+for _ in range(N):
+    num = int(input())
+    if num == 0:
+        if heap: print(heapq.heappop(heap)[1])
+        else: print(0)
+    else: heapq.heappush(heap, (-num, num))
