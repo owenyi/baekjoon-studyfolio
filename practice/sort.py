0c01f1aa58dcc360d22f3206d3413442d752a374
owenyi <owenyi3207@gmail.com>
2022-04-02 11:16:44 +0900

    update sort practice

--- practice/sort.py
+++ practice/sort.py
@@ -1,3 +1,11 @@
+aList = ["banana", "apple", "kiwi"]
+
+aList.sort()  # 사전 순서
+print(aList)
+
+aList.sort(key=len)  # 길이가 작은 것부터
+print(aList)
+
 a = [[4, 4], [3, 1], [3, 2], [2, 3], [1, 1]]
 print(a)
 
